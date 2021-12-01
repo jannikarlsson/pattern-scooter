8c1511f49e32185055173d9a3a13a4406a414ab7
datalowe <datalowe@posteo.de>
2021-12-01 12:12:10 +0100

    add functions for calculating geographical distances and related values (most importantly, 'calculate_step_lat_lon') . add tests directory and populate it with geo-related tests.

---
+++ distance_calc/distance_constants.py
@@ -0,0 +1,24 @@
+"""
+Constants used in distance calculations.
+"""
+# https://en.wikipedia.org/wiki/Earth_radius
+EARTH_RADIUS_KM = 6371
+# Skövde center according to Google Maps, and
+# city zone radius in kilometers
+SKOVDE_GEO = {
+    "center_lat": 59.8332051,
+    "center_lon": 17.5183649,
+    "radius": 5
+}
+LUND_GEO = {
+    "center_lat": 55.7106955,
+    "center_lon": 13.2013123,
+    "radius": 2
+}
+UPPSALA_GEO = {
+    "center_lat": 59.8615337,
+    "center_lon": 17.6543391,
+    "radius": 5
+}
+
+# -- END CONSTANTS --
